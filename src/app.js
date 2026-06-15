9fac078a4b0f066370b3930142c6e96505d1f636
Nairouz Bakhta BEN HADJ SLAMA <152972131+bakhtaa@users.noreply.github.com>
2026-06-15 14:08:20 +0100

    adding https requests

--- src/app.js
+++ src/app.js
@@ -2,7 +2,19 @@ const express= require('express');
 
 const app = express();
 
+//this will match the get api call
+app.get("/users", (req,res)=>{
+    res.send({name: 'bakhtaa'})
+});
+app.post("/users", (req,res)=>{
+    res.send("added to dbb")
+});
 
+app.delete("/users", (req,res)=>{
+    res.send("deleted")
+});
+
+//this will match all the api calls 
 app.use("/",(req,res)=>{
     res.send("wiwaa wiwaa");
 })
