f998369691e57123a0f3c897a33f0a327429e927
Nairouz Bakhta BEN HADJ SLAMA <152972131+bakhtaa@users.noreply.github.com>
2026-06-25 19:17:21 +0100

    document added to db

--- src/app.js
+++ src/app.js
@@ -5,12 +5,37 @@ const app = express();
 
 const User= require('./models/user');
 
-app.post("/signup", (req, res )=>{
+app.use(express.json());
+//now our middleware express.json() will be activated for all the routes
+//app.use(callback function)
+//this  app.use() will run on every request that comes to our server
+//if i give it some route : app.use(/route1,callback function)
+//then this will only work for route1 and not all routes but for all type of requests
+
+app.post("/signup", async(req, res )=>{
 // bech nekhdhou ldata stocké fel req w nhotouha fel basa
 //nasn3ou instance mta3 lmodel user nostockiw feha data li jetna mel req
+    console.log("Signup route hit");
+console.log(req.body);
+//data in sent to the poster in the body of a post request but our express server is unable to read
+//json data, to be able to read json data we have to use a middleware , this is why when we write 
+//console.log(req.body) we get undefined
+//we need a middleware because it's a function that's gonna be used for all the apis 
+//it is used to convert the json object into a javascript object so we can use that data into the code
 
-const user= new User({});
+const user= new User(req.body);
+try{
+await user.save();
+res.send("user signed in yee yee");
+}
+catch(err){
+    res.status(400).send("error occured"+ err.message);
+}
 })
+//this will save the data to the database 
+//the save function will return a promise this is why you should use async await 
+
+
 
 
 
