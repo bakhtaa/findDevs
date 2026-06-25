cc2f99a4d6ab10c41fa659c9d0400502ff5f2f05
Nairouz Bakhta BEN HADJ SLAMA <152972131+bakhtaa@users.noreply.github.com>
2026-06-25 13:56:29 +0100

    signup api

--- src/app.js
+++ src/app.js
@@ -3,11 +3,25 @@ const connect= require('./config/database');
 
 const app = express();
 
+const User= require('./models/user');
+
+app.post("/signup", (req, res )=>{
+// bech nekhdhou ldata stocké fel req w nhotouha fel basa
+//nasn3ou instance mta3 lmodel user nostockiw feha data li jetna mel req
+
+const user= new User({});
+})
+
+
+
+
 connect().then(()=>{console.log("connected babby");
+//once your database is successfullly connected then you should do your app.listen(), 
+//ceci assure que le client mayadhrabch aa serveur wel basa mezelt mehich connecté
 app.listen(3000, ()=>{
     console.log("server running");
 });
 }).catch(err=>{
-    console.error("etissal maktouu3");
+    console.error("etissal maktou3");
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -22,6 +22,6 @@ const userSchema= mongoose.Schema(
         }
     }
 )
-const User= mongoose.model("User", userSchema);
+const userModel= mongoose.model("User", userSchema);
 
 module.exports=userModel;
