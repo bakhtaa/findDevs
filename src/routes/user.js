4a4d738b36d794c5ef036f083f9366b544d4476e
Nairouz Bakhta BEN HADJ SLAMA <152972131+bakhtaa@users.noreply.github.com>
2026-07-16 16:59:22 +0100

    getConnections & getRequests apis added

---
+++ src/routes/user.js
@@ -0,0 +1,60 @@
+//
+//GET /user/connections
+//GET /user/requests
+//GET /user/feed get the profiles of the other users on the plateform 
+
+
+
+//first route is GET /user/connections
+
+
+const {userAuth}= require("../middlewares/auth");
+const express = require('express');
+
+const userRouter= express.Router();
+const ConnectionRequest= require('../models/connectionRequest');
+
+
+
+userRouter.get("/user/connections", userAuth, async (req,res)=>{
+    try{
+ //le logged in user veut voir la liste des coonnections qu'il a 
+    //donc on doit extraire de la base de données tout les documents ConnectionRequest 
+    //dont le status est accepted 
+    //ceci est donc un tableau de connections qu'on doit renvoyer
+    const connections= await  ConnectionRequest.find({status: "accepted"}); 
+    //ceci veut dire renvoie moi toutes les connections dont le status est accepted
+    //maintenant que nous avons un tableau connections 
+    //il reste juste de le renvoyer avec la response
+    res.json({connections});
+    //voila le travail fait sans verification
+    }
+    catch(err){
+        res.send("an error occured somewhere "+ err);
+    }
+   
+})
+
+userRouter.get("/user/requests", userAuth, async (req,res)=>{
+
+    try {
+//le logged in user veut voir la liste des coonnections qu'il a 
+    //donc on doit extraire de la base de données tout les documents ConnectionRequest 
+    //dont le status est accepted 
+    //ceci est donc un tableau de connections qu'on doit renvoyer
+    const requests= await ConnectionRequest.find({status: "interested"}); 
+    //ceci veut dire renvoie moi toutes les connections dont le status est accepted
+    //maintenant que nous avons un tableau connections 
+    //il reste juste de le renvoyer avec la response
+    res.json({requests});
+    //voila le travail fait sans verification
+    }
+    catch(err){
+        res.send("an error occured somewhere "+ err);
+    }
+    
+})
+
+
+
+
